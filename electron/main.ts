06be36a2a5b45b2a4ac653fe8ed6ef44ef60ce3a
Nassist75010 <sofianeboulot03@gmail.com>
2026-07-26 15:18:08 +0200

    Ajouter la fenêtre Electron sécurisée

---
+++ electron/main.ts
@@ -0,0 +1,37 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'node:path';
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const window = new BrowserWindow({
+    width: 1440,
+    height: 920,
+    minWidth: 1100,
+    minHeight: 720,
+    title: "N'Assist Manager Pro",
+    backgroundColor: '#08101d',
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  if (isDev) {
+    void window.loadURL('http://localhost:5173');
+  } else {
+    void window.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
